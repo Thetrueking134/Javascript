d7e0a1dd87d280320e173c0a42a6e3a39dc99b08
Imran <imran.t.majidli@gmail.com>
2018-12-11 23:57:01 +0400

    Updated

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-function First(){
-Channel('Yummy','challenge-6-star');
+function First() {
+	Channel('Yummy', 'challenge-6-star');
 }
 
-function Second(){
-Channel('SevenContinents','challenge-6-star-o');
+function Second() {
+	Channel('SevenContinents', 'challenge-6-star-o');
 }
 
-function Third(){
-Channel('KillerApp','challenge-6-star');
+function Third() {
+	Channel('KillerApp', 'challenge-6-star');
 }
 
-function Fourth(){
-Channel('FirstPersonOnMars','challenge-6-star-o');
+function Fourth() {
+	Channel('FirstPersonOnMars', 'challenge-6-star-o');
 }
 
-function Fifth(){
-Channel('Octoberfest','challenge-6-star');
-}
-
-
-function Channel(Name,image){
-$('#bar').html('#' + Name + ' by <span id="ID"><a href="https://map.what3words.com/upgrading.never.helps" target="_blank"><small style="color:white">upgrading.never.helps</small></span>');
-$('#Image').attr('src','img/' + image + '.png');
+function Fifth() {
+	Channel('Octoberfest', 'challenge-6-star');
 }
 
 
+function Channel(Name, image) {
+	$('#bar').html('#' + Name + ' by <span id="ID"><a href="https://map.what3words.com/upgrading.never.helps" target="_blank"><small style="color:white">upgrading.never.helps</small></span>');
+	$('#Image').attr('src', 'img/' + image + '.png');
+}
+
+function Return() {
+	$('#Image').attr('src', 'img/challenge-6-star.png');
+}
